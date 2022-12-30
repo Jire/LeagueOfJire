02924af397db80a89aa890c07aa0dd66f380c756
Jire <thomasgnappo@gmail.com>
2022-12-30 01:24:03 -0500

    Script standalone configuration for KTS in Kotlin `1.8.0` support

--- scripts/src/main/kotlin/com/leagueofjire/scripts/MyScriptCompilationConfiguration.kt
+++ scripts/src/main/kotlin/com/leagueofjire/scripts/MyScriptCompilationConfiguration.kt
@@ -21,21 +21,22 @@ package com.leagueofjire.scripts
 import kotlin.script.experimental.api.*
 
 internal object MyScriptCompilationConfiguration : ScriptCompilationConfiguration({
-	defaultImports(
-		"java.awt.event.KeyEvent",
-		"com.badlogic.gdx.graphics.Color",
-		"com.leagueofjire.ScreenPosition",
-		"com.leagueofjire.util.*",
-		"com.leagueofjire.input.*",
-		"com.leagueofjire.overlay.*",
-		"com.leagueofjire.game.*",
-		"com.leagueofjire.game.unit.*",
-		"com.leagueofjire.game.unit.champion.*",
-		"com.leagueofjire.game.unit.champion.item.*",
-		"com.leagueofjire.game.unit.champion.spell.*",
-		"com.leagueofjire.game.unit.champion.spell.GameChampionSpells.*",
-	)
-	ide {
-		acceptedLocations(ScriptAcceptedLocation.Everywhere)
-	}
+    defaultImports(
+        "java.awt.event.KeyEvent",
+        "com.badlogic.gdx.graphics.Color",
+        "com.leagueofjire.ScreenPosition",
+        "com.leagueofjire.util.*",
+        "com.leagueofjire.input.*",
+        "com.leagueofjire.overlay.*",
+        "com.leagueofjire.game.*",
+        "com.leagueofjire.game.unit.*",
+        "com.leagueofjire.game.unit.champion.*",
+        "com.leagueofjire.game.unit.champion.item.*",
+        "com.leagueofjire.game.unit.champion.spell.*",
+        "com.leagueofjire.game.unit.champion.spell.GameChampionSpells.*",
+    )
+    ide {
+        acceptedLocations(ScriptAcceptedLocation.Everywhere)
+    }
+    isStandalone(true)
 })
